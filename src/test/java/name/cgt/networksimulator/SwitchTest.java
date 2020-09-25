5ffc455b1a9d85069b16f18e140f07a6ff9424c1
Christoffer G. Thomsen <chris@cgt.name>
2020-09-25 14:39:57 +0200

    A switch forwards frames to a Link

---
+++ src/test/java/name/cgt/networksimulator/SwitchTest.java
@@ -0,0 +1,33 @@
+package name.cgt.networksimulator;
+
+import org.jmock.Expectations;
+import org.jmock.junit5.JUnit5Mockery;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+public class SwitchTest {
+    @RegisterExtension
+    final JUnit5Mockery context = new JUnit5Mockery();
+
+    @Test
+    public void forward_frames_to_link() {
+        final var link = context.mock(Link.class);
+        final var expectedFrame = new Frame(new NetworkAdapterAddress(), new NetworkAdapterAddress(), null);
+
+        context.checking(new Expectations() {{
+            oneOf(link).onFrame(with(equal(expectedFrame)));
+        }});
+
+        final Link switch_ = new Link() {
+            @Override
+            public void onConnected(FrameListener frameListener) {
+            }
+
+            @Override
+            public void onFrame(Frame frame) {
+                link.onFrame(frame);
+            }
+        };
+        switch_.onFrame(expectedFrame);
+    }
+}
